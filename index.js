0cbb06b435af06369672544943c2c560f7f8df9e
Kristófer Fannar Björnsson <kristofer.fannar.bjornsson@hafogvatn.is>
2023-01-23 14:31:03 +0000

    added clear button, styling and fixed init bugs

--- index.js
+++ index.js
@@ -1,22 +1,103 @@
+function init() {
+    firstNumber = undefined;
+    lastNumber = undefined;
+    lastOperator = NONE;
+    clearScreen = true;
+    lastButtonPressed = NONE;
+}
+
 function buttonPressed(button) {
-    screen.textContent += button.textContent;
+    if (clearScreen) {
+        screen.textContent = "";
+        clearScreen = false;
+    }
+
+    if (lastOperator === NONE || !button.classList.contains('operator')) {
+        screen.textContent += button.textContent;
+    }
 }
 
 function numberPressed(number) {
-    lastNumber = number;
+    if (lastButtonPressed == NUMBER) {
+        lastNumber = lastNumber * 10 + number;
+    } else {
+        firstNumber = lastNumber;
+        lastNumber = number;
+        lastButtonPressed = NUMBER;
+    }
 }
 
 function operatorPressed(operator) {
-    lastOperator = operator.textContent;
+    if (lastOperator !== NONE)  {
+        alert("operator already used");
+    } else {
+        lastOperator = operator.textContent;
+        lastButtonPressed = lastOperator;
+    }
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        alert("dividing by zero is really not a great idea");
+        return "ERROR";
+    }
+    
+    return a / b;
+}
+
+function calculate() {
+    let result;
+    if (lastOperator == PLUS) {
+        result = add(firstNumber, lastNumber);
+    } else if (lastOperator == MINUS) {
+        result = subtract(firstNumber, lastNumber);
+    } else if (lastOperator == MULTIPLY) {
+        result = multiply(firstNumber, lastNumber);
+    } else {
+        result = divide(firstNumber, lastNumber);
+    }
+
+    if (isNaN(result)) {
+        alert("now you're really being nitpicky");
+        result = "ERROR";
+    }
+
+    screen.textContent = result;
+    init();
+}
+
+function clear() {
+    screen.textContent = "";
+    clearScreen = false;
+}
+
+
 
 const PLUS = "+";
 const MINUS = "-";
 const DIVIDE = "/";
 const MULTIPLY = "*";
-let lastOperator;
-let lastNumber;
+const NONE = " ";
+const NUMBER = "number";
 
+let lastOperator;
+let firstNumber;
+let lastNumber;
+let clearScreen;
+let lastButtonPressed;
+init();
 
 const screen = document.querySelector(".screen");
 
@@ -25,7 +106,13 @@ buttons.forEach(button => button.addEventListener('click', () => buttonPressed(b
 
 
 const numbers = document.querySelectorAll(".number");
-numbers.forEach(number => number.addEventListener("click", () => numberClicked(+number.textContent)));
+numbers.forEach(number => number.addEventListener("click", () => numberPressed(+number.textContent)));
 
 const operators = document.querySelectorAll(".operator");
 operators.forEach(operator => operator.addEventListener("click", () => operatorPressed(operator)));
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", () => calculate());
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => clear());
